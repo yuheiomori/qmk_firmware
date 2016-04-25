b4cdc6402caecf6377f6223dcc2eb02f8e4ab158
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2016-04-25 23:58:11 +0900

    first build test of modkc_t

---
+++ keyboard/ergodox_ez/additional_keycode.h
@@ -0,0 +1,25 @@
+//    Copyright 2016 Yoshi Yamaguchi
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+/*
+ * additional keycode added by ymotongpoo.
+ */
+
+#ifndef ADDITIONAL_KEYCODE_H
+#define ADDITIONAL_KEYCODE_H
+
+#include "keymap_common.h"
+
+#define CTLKC_T(kc1, kc2) MT(kc2 | 0x7000 | ((CTL(kc1) & 0xF) << 8))
+#define GUIKC_T(kc1, kc2) MT(kc2 | 0x7000 | ((MOD(kc1) & 0xF) << 8))
--- keyboard/ergodox_ez/keymaps/default/keymap.c
+++ keyboard/ergodox_ez/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 // https://github.com/msc654/qmk_firmware/blob/master/keyboard%2Fergodox_ez%2Fkeymaps%2Fdefault%2Fkeymap.c
 
 #include "ergodox_ez.h"
+#include "additoinal_keycode.h"
 #include "debug.h"
 #include "action_layer.h"
 
@@ -17,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Tab  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
+ * | CTRL   |A/G+A |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
  * |--------+------+------+------+------+------| LGui |           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -35,10 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [OSX] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TAB,
-        KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
+        KC_EQL,         KC_1,                 KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
+        KC_TAB,         KC_Q,                 KC_W,   KC_E,   KC_R,   KC_T,   KC_TAB,
+        KC_LCTL,        GUIKC_T(KC_A, KC_A),  KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,                 KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
         TO(SYMB,1),     LGUI(KC_V),   LGUI(KC_C),   KC_LEFT,KC_RGHT,
                                                KC_LGUI,       KC_LALT,
                                                               KC_HOME,
