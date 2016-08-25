fe941c57311cfc698cb0af5f12333cb5e5f950bb
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2016-08-25 15:20:07 +0900

    fix typo

--- keyboards/ergodox/keymaps/ymotongpoo/keymap.c
+++ keyboards/ergodox/keymaps/ymotongpoo/keymap.c
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
                                                             KC_TRNS, KC_TRNS,
                                                                      KC_TRNS,
-                                      LGU(LSFT(KC_A)), LALT(KC_ENT), KC_TRNS,
+                                     LGUI(LSFT(KC_A)), LALT(KC_ENT), KC_TRNS,
        // right hand
        KC_F7,     KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
