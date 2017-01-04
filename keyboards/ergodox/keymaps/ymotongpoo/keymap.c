4be333c296c46600182e31e8a1240bb7472d78ff
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-01-04 12:33:16 +0900

    fix

--- keyboards/ergodox/keymaps/ymotongpoo/keymap.c
+++ keyboards/ergodox/keymaps/ymotongpoo/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              LALT(KC_RIGHT), KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                              KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              MO(WINFN),      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                    KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          TO(OSX,1),
+                                    KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          TO(MAC,1),
              KC_LALT,        KC_ESC,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
