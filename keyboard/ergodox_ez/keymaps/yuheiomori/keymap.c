00664d0a9037d4dc2a5bd3db7eec6f92ad5b6c36
Yuhei Omori <yuhei.omori@beproud.jp>
2017-01-15 17:40:49 +0900

    fix typo

--- keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_6,         KC_7,       KC_8,    KC_9,    KC_0,             KC_EQL,
         KC_NO,          KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,             KC_MINS,
                         KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,          KC_QUOT,
-        LT(UBUNTUFN, NO),   KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,          KC_BSLS,
+        LT(UBUNTUFN, KC_NO),   KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,          KC_BSLS,
         KC_LALT,        KC_APP,       KC_LBRC,    KC_RBRC,          TO(MAC, 1),
                                                   KC_LEFT, KC_RGHT,
                                                   KC_UP,
