86f45afed89a55afa3e08e0810b3272a371f1ab8
Yuhei Omori <yuhei.omori@beproud.jp>
2017-01-15 15:44:28 +0900

    fix compile error

--- keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -232,9 +232,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-
-};
-
 const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_TOGGLE(UBUNTUFN),
   [2] = ACTION_LAYER_TAP_TOGGLE(MACFN)
