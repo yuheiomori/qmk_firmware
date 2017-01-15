52cdcbe3e7eee11ae7156e58d16629855d8b674b
Yuhei Omori <yuhei.omori@beproud.jp>
2017-01-15 15:42:42 +0900

    fix compile error

--- keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -63,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |  K   |   L  |   ;  | '"/GUI |
+ * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |  K   |   L  |   ;  | '"     |
  * |--------+------+------+------+------+------| LFn  |           | LFn  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl|\/Shift |
+ * | LShift |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |  \     |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | cut  | paste| copy | Alt  | LGUI |                                       | RGUI |      | [    | ]    |  L1  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_1,         KC_2,       KC_3,    KC_4,   KC_5,   KC_GRV,
         KC_TAB,         KC_Q,         KC_W,       KC_E,    KC_R,   KC_T,   KC_NO,
         KC_LCTL,        KC_A,         KC_S,       KC_D,    KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,       KC_C,    KC_V,   KC_B,   MO(MACFN),
+        KC_LSFT,        KC_Z,         KC_X,       KC_C,    KC_V,   KC_B,   MO(MACFN),
         LGUI(KC_X),     LGUI(KC_V),   LGUI(KC_C), KC_LALT, KC_LGUI,
                                                   LSFT(LCTL(KC_TAB)),   LCTL(KC_TAB),
                                                            KC_HOME,
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_GRV,         KC_6,         KC_7,       KC_8,    KC_9,    KC_0,             KC_EQL,
         KC_NO,          KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,             KC_MINS,
-                        KC_H,         KC_J,       KC_K,    KC_L,    LT(MDIA, KC_SCLN),         GUI_T(KC_QUOT),
-        MO(MACFN),      KC_N,         KC_M,       KC_COMM, KC_DOT,  CTL_T(KC_SLSH),   SFT_T(KC_BSLS),
+                        KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,          KC_QUOT,
+        MO(MACFN),      KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,          KC_BSLS,
         KC_RGUI,        KC_NO,        KC_LBRC,    KC_RBRC,          TO(UBUNTU,1),
                                                   KC_LEFT, KC_RGHT,
                                                   KC_UP,
