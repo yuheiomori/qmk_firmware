cfb6f12e8929e7ae61ca590c5fede774cf58ff28
Yuhei Omori <yuhei.omori@beproud.jp>
2017-07-08 09:33:53 +0900

    remove RESET

--- keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboard/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 3: Ubuntu function Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 | RESET  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |  up  |      |      |      |      |           |      |      |  7   |  8   |  9   |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_TRNS,
                                                 KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
-       KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,
+       KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
        KC_TRNS,   KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_TRNS ,  KC_TRNS,
                 KC_TRNS,  KC_4,    KC_5,    KC_6,    KC_TRNS,   KC_TRNS,
        KC_TRNS,   KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_TRNS,   KC_TRNS,
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 4: Mac function Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 | RESET  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |  up  |      |      |      |      |           |      |      |  7   |  8   |  9   |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -177,7 +177,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TRNS,
                  KC_TRNS, KC_TRNS, KC_TRNS,
                  // right hand
-                 KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  RESET,
+                 KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
                  KC_TRNS,   KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_TRNS ,  KC_TRNS,
                  KC_TRNS,  KC_4,    KC_5,    KC_6,    KC_TRNS,   KC_TRNS,
                  KC_TRNS,   KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_TRNS,   KC_TRNS,
