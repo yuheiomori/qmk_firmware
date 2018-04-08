f00504e25439499defb109e844f692569e96f514
Yuhei Omori <yuhei.omori@beproud.jp>
2018-04-08 18:39:20 +0900

    fix typo

--- keyboards/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboards/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -136,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
        KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LANG1,
-       KC_TRNS,   KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_TRNS ,  KCLANG2,
+       KC_TRNS,   KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_TRNS ,  KC_LANG2,
                 KC_TRNS,  KC_4,    KC_5,    KC_6,    KC_TRNS,   KC_TRNS,
        KC_TRNS,   KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_TRNS,   KC_TRNS,
                 KC_0,   KC_TRNS,  KC_TRNS,    KC_TRNS,   TO(MOUSE),
