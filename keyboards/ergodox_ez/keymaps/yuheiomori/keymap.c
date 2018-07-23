e3cd91165ad59b1dcad29038753e3f0b9a21d409
Yuhei Omori <omori.yuhei@panair.jp>
2018-07-23 09:46:57 +0900

    MAC first

--- keyboards/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboards/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -3,15 +3,62 @@
 #include "debug.h"
 #include "action_layer.h"
 
-#define UBUNTU 0 // default layer
-#define MAC 1
-#define UBUNTUFN 2
-#define MACFN 3
+#define MAC 0 // default layer
+#define UBUNTU 1
+#define MACFN 2
+#define UBUNTUFN 3
 #define MOUSE 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Keymap 0: UBUNTU
+
+
+/* Keymap 0: Mac
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * | ESC    |   1  |   2  |   3  |   4  |   5  | `~   |           | `~   |   6  |   7  |   8  |   9  |   0  |   =    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |  K   |   L  |   ;  | '"     |
+ * |--------+------+------+------+------+------| LFn  |           | LFn  |------+------+------+------+------+--------|
+ * | LShift |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |  \     |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | cut  | paste| copy | Alt  | LGUI |                                       | RGUI |      | [    | ]    |  L1  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | Home | End  |       | left |right |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | PgUp |       | Up   |        |      |
+ *                                 |Space |Bask  |----- |       |----- |        |Enter |
+ *                                 |      |Space | PgDn |       | Down |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+
+[MAC] = KEYMAP(
+        // left hand
+        KC_ESC,         KC_1,         KC_2,       KC_3,    KC_4,   KC_5,   KC_GRV,
+        KC_TAB,         KC_Q,         KC_W,       KC_E,    KC_R,   KC_T,   KC_NO,
+        KC_LCTL,        KC_A,         KC_S,       KC_D,    KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,       KC_C,    KC_V,   KC_B,   MO(MACFN),
+        LGUI(KC_X),     LGUI(KC_V),   LGUI(KC_C), KC_LALT, KC_LGUI,
+                                                           KC_HOME, KC_END,
+                                                                    KC_PGUP,
+                                                  KC_SPC,  KC_BSPC, KC_PGDOWN,
+
+        // right hand
+        KC_GRV,         KC_6,         KC_7,       KC_8,    KC_9,    KC_0,             KC_EQL,
+        KC_NO,          KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,             KC_MINS,
+                        KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,          KC_QUOT,
+        MO(MACFN),      KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,          KC_BSLS,
+        KC_RGUI,        KC_NO,        KC_LBRC,    KC_RBRC,          TO(UBUNTU),
+                                                  KC_LEFT, KC_RGHT,
+                                                  KC_UP,
+                                                  KC_DOWN, KC_NO ,  KC_ENT
+    ),
+
+
+/* Keymap 1: UBUNTU
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | ESC    |   1  |   2  |   3  |   4  |   5  | `~   |           | `~   |   6  |   7  |   8  |   9  |   0  |   =    |
@@ -57,95 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                   KC_DOWN, KC_NO ,  KC_ENT
     ),
 
-/* Keymap 1: Mac
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  | `~   |           | `~   |   6  |   7  |   8  |   9  |   0  |   =    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   -    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CTRL   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |  K   |   L  |   ;  | '"     |
- * |--------+------+------+------+------+------| LFn  |           | LFn  |------+------+------+------+------+--------|
- * | LShift |  Z   |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |  \     |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | cut  | paste| copy | Alt  | LGUI |                                       | RGUI |      | [    | ]    |  L1  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | Home | End  |       | left |right |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | PgUp |       | Up   |        |      |
- *                                 |Space |Bask  |----- |       |----- |        |Enter |
- *                                 |      |Space | PgDn |       | Down |        |      |
- *                                 `--------------------'       `----------------------'
- */
-
-[MAC] = KEYMAP(
-        // left hand
-        KC_ESC,         KC_1,         KC_2,       KC_3,    KC_4,   KC_5,   KC_GRV,
-        KC_TAB,         KC_Q,         KC_W,       KC_E,    KC_R,   KC_T,   KC_NO,
-        KC_LCTL,        KC_A,         KC_S,       KC_D,    KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,       KC_C,    KC_V,   KC_B,   MO(MACFN),
-        LGUI(KC_X),     LGUI(KC_V),   LGUI(KC_C), KC_LALT, KC_LGUI,
-                                                           KC_HOME, KC_END,
-                                                                    KC_PGUP,
-                                                  KC_SPC,  KC_BSPC, KC_PGDOWN,
-
-        // right hand
-        KC_GRV,         KC_6,         KC_7,       KC_8,    KC_9,    KC_0,             KC_EQL,
-        KC_NO,          KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,             KC_MINS,
-                        KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,          KC_QUOT,
-        MO(MACFN),      KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,          KC_BSLS,
-        KC_RGUI,        KC_NO,        KC_LBRC,    KC_RBRC,          TO(UBUNTU),
-                                                  KC_LEFT, KC_RGHT,
-                                                  KC_UP,
-                                                  KC_DOWN, KC_NO ,  KC_ENT
-    ),
-
-
-/* Keymap 3: Ubuntu function Layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 | LANG1  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |  up  |      |      |      |      |           |      |      |  7   |  8   |  9   |      | LANG2  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | left | down |right |      |      |------|           |------|      |  4   |  5   |  6   |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |  1   |  2   |  3   |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |  0   |      |      |      |LMouse|
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      | Mute |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------| Vol- | Vol+ |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-// Functions
-[UBUNTUFN] = KEYMAP(
-       // left hand
-       KC_TRNS,     KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,
-       KC_TRNS,     KC_TRNS,    KC_UP,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,
-       KC_TRNS,     KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS,   KC_TRNS,
-       KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,
-       KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
-                                                       KC_TRNS, KC_TRNS,
-                                                              KC_TRNS,
-                                                KC_TRNS, KC_TRNS, KC_TRNS,
-       // right hand
-       KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LANG1,
-       KC_TRNS,   KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_TRNS ,  KC_LANG2,
-                KC_TRNS,  KC_4,    KC_5,    KC_6,    KC_TRNS,   KC_TRNS,
-       KC_TRNS,   KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_TRNS,   KC_TRNS,
-                KC_0,   KC_TRNS,  KC_TRNS,    KC_TRNS,   TO(MOUSE),
-       KC_TRNS,   KC_MUTE,
-       KC_TRNS,
-       KC_TRNS,   KC_VOLD, KC_VOLU
-    ),
-
-/* Keymap 4: Mac function Layer
+/* Keymap 3: Mac function Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 |        |
@@ -187,6 +146,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TRNS,
                  KC_TRNS,   KC_VOLD, KC_VOLU
                  ),
+
+
+
+/* Keymap 4: Ubuntu function Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F6   |           | F7   |  F8  |  F9  | F10  |  F11 |  F12 | LANG1  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |  up  |      |      |      |      |           |      |      |  7   |  8   |  9   |      | LANG2  |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        | left | down |right |      |      |------|           |------|      |  4   |  5   |  6   |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |  1   |  2   |  3   |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |  0   |      |      |      |LMouse|
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      | Mute |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------| Vol- | Vol+ |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// Functions
+[UBUNTUFN] = KEYMAP(
+       // left hand
+       KC_TRNS,     KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,
+       KC_TRNS,     KC_TRNS,    KC_UP,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,
+       KC_TRNS,     KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS,   KC_TRNS,
+       KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS, KC_TRNS,
+       KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+                                                       KC_TRNS, KC_TRNS,
+                                                              KC_TRNS,
+                                                KC_TRNS, KC_TRNS, KC_TRNS,
+       // right hand
+       KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LANG1,
+       KC_TRNS,   KC_TRNS,  KC_7,    KC_8,    KC_9,    KC_TRNS ,  KC_LANG2,
+                KC_TRNS,  KC_4,    KC_5,    KC_6,    KC_TRNS,   KC_TRNS,
+       KC_TRNS,   KC_TRNS,  KC_1,    KC_2,    KC_3,    KC_TRNS,   KC_TRNS,
+                KC_0,   KC_TRNS,  KC_TRNS,    KC_TRNS,   TO(MOUSE),
+       KC_TRNS,   KC_MUTE,
+       KC_TRNS,
+       KC_TRNS,   KC_VOLD, KC_VOLU
+    ),
 
 /* Keymap 5: Mouse Layer
  *
@@ -276,26 +280,30 @@ void matrix_scan_user(void) {
     ergodox_right_led_3_off();
     switch (layer) {
       // TODO: Make this relevant to the ErgoDox EZ.
-        case UBUNTU:
-          ergodox_right_led_1_on();
-          ergodox_right_led_2_off();
-          ergodox_right_led_3_off();
-          break;
         case MAC:
           ergodox_right_led_1_off();
           ergodox_right_led_2_on();
           ergodox_right_led_3_off();
           break;
-        case UBUNTUFN:
-          ergodox_right_led_1_on();
-          ergodox_right_led_2_off();
-          ergodox_right_led_3_on();
-          break;
+
         case MACFN:
           ergodox_right_led_1_off();
           ergodox_right_led_2_on();
           ergodox_right_led_3_on();
           break;
+
+        case UBUNTU:
+          ergodox_right_led_1_on();
+          ergodox_right_led_2_off();
+          ergodox_right_led_3_off();
+          break;
+
+        case UBUNTUFN:
+          ergodox_right_led_1_on();
+          ergodox_right_led_2_off();
+          ergodox_right_led_3_on();
+          break;
+
         case MOUSE:
           ergodox_right_led_1_on();
           ergodox_right_led_2_on();
