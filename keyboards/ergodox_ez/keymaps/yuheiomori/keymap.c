72a8f821a0c452b5fa42b021990a62edbdd8803a
Yuhei Omori <omori.yuhei@panair.jp>
2019-12-13 18:40:43 +0900

    add spreadsheet shortcut

--- keyboards/ergodox_ez/keymaps/yuheiomori/keymap.c
+++ keyboards/ergodox_ez/keymaps/yuheiomori/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC] = KEYMAP(
         // left hand
         KC_ESC,         KC_1,         KC_2,       KC_3,    KC_4,   KC_5,   KC_GRV,
-        KC_TAB,         KC_Q,         KC_W,       KC_E,    KC_R,   KC_T,   KC_NO,
+        KC_TAB,         KC_Q,         KC_W,       KC_E,    KC_R,   KC_T,   LGUI(LSFT(KC_PGUP)),
         KC_LCTL,        KC_A,         KC_S,       KC_D,    KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,       KC_C,    KC_V,   KC_B,   MO(MACFN),
         LGUI(KC_X),     LGUI(KC_V),   LGUI(KC_C), KC_LALT, KC_LGUI,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // right hand
         KC_GRV,         KC_6,         KC_7,       KC_8,    KC_9,    KC_0,             KC_EQL,
-        KC_NO,          KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,             KC_MINS,
+        LGUI(LSFT(KC_PGDOWN)),          KC_Y,         KC_U,       KC_I,    KC_O,    KC_P,             KC_MINS,
                         KC_H,         KC_J,       KC_K,    KC_L,    KC_SCLN,          KC_QUOT,
         MO(MACFN),      KC_N,         KC_M,       KC_COMM, KC_DOT,  KC_SLSH,          KC_BSLS,
         KC_RGUI,        KC_NO,        KC_LBRC,    KC_RBRC,          TO(UBUNTU),
